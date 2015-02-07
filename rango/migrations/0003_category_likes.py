a60ae6bdfd81f40b118236af9fd6f0c4601fcb3b
SanketDG <sanketdasgupta@gmail.com>
2015-02-08 01:06:39 +0530

    add migrations

---
+++ rango/migrations/0003_category_likes.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0002_auto_20150203_1908'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='category',
+            name='likes',
+            field=models.IntegerField(default=0),
+            preserve_default=False,
+        ),
+    ]
