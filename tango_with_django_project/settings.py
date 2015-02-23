f82406546dd68463d182f2729e041790b3bf2dfa
SanketDG <sanketdasgupta@gmail.com>
2015-02-24 00:00:25 +0530

    add registration-redux

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -92,3 +93,13 @@ STATICFILES_DIRS = (
 )
 
 LOGIN_URL = '/rango/login/'
+
+REGISTRATION_OPEN = True                # If True, users can register
+# One-week activation window; you may, of course, use a different value.
+ACCOUNT_ACTIVATION_DAYS = 7
+# If True, the user will be automatically logged in.
+REGISTRATION_AUTO_LOGIN = True
+# The page you want users to arrive at after they successful log in
+LOGIN_REDIRECT_URL = '/rango/'
+# The page users are directed to if they are not logged in,
+LOGIN_URL = '/accounts/login/'
