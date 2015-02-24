a80ed5c550413f7984f9905f5c3be102eb18af9c
SanketDG <sanketdasgupta@gmail.com>
2015-02-24 23:42:11 +0530

    fix typo

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^rango/', include('rango.urls')),
                        url(r'^accounts/',
-                           include('accounts.backends.simple.urls')),
+                           include('registration.backends.simple.urls')),
                        )
