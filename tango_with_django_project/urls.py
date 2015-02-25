6d6a179b4660dd1360e7a607ffc14c2000844f6b
SanketDG <sanketdasgupta@gmail.com>
2015-02-25 23:54:03 +0530

    redirect to index after registration

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from registration.backends.simple.views import MyRegistrationView
 
 urlpatterns = patterns('',
                        # Examples:
@@ -10,4 +11,5 @@ urlpatterns = patterns('',
                        url(r'^rango/', include('rango.urls')),
                        url(r'^accounts/',
                            include('registration.backends.simple.urls')),
+                       url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
                        )
