15094a71a07e98e92faa3d89eefb996a8fffc6aa
SanketDG <sanketdasgupta@gmail.com>
2015-03-08 23:33:54 +0530

    create class to redirect to /rango

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from registration.backends.simple.views import MyRegistrationView
+from registration.backends.simple.views import RegistrationView
+
+
+class MyRegistrationView(RegistrationView):
+
+    def get_success_url(self, request, user):
+        return '/rango/'
 
 urlpatterns = patterns('',
                        # Examples:
@@ -11,5 +17,6 @@ urlpatterns = patterns('',
                        url(r'^rango/', include('rango.urls')),
                        url(r'^accounts/',
                            include('registration.backends.simple.urls')),
-                       url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+                       url(r'^accounts/register/$', MyRegistrationView.as_view(),
+                           name='registration_register'),
                        )
